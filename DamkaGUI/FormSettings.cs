d7116aa602af2354103ffeb5c1b0757d3052f22f
or keren <orkeren8@gmail.com>
2025-02-01 10:08:16 +0200

    add players section in form

--- DamkaGUI/FormSettings.cs
+++ DamkaGUI/FormSettings.cs
@@ -9,10 +9,18 @@ namespace DamkaGUI
 {
     internal class FormSettings : Form
     {
-        Label m_LabelBoardSize = new Label();
-        private Dictionary<string, RadioButton> m_RadioButtonsBoardSize = new Dictionary<string, RadioButton>();
+        private readonly int r_VeticalMergin = 25;
+        private readonly int r_HorizontalMargin = 25;
+        private Label LabelBoardSize { get; set; } = new Label();
+        private Dictionary<string, RadioButton> RadioButtonsBoardSize { get; set; } = new Dictionary<string, RadioButton>();
+        private Label LabelPlayers { get; set; } = new Label();
+        private Label LabelPlayer1 { get; set; } = new Label();
+        private CheckBox CheckBoxPlayer2 { get; set; } = new CheckBox();
+        private TextBox Player1Name {get; set; } = new TextBox();
+        private TextBox Player2Name {get; set; } = new TextBox();
+
         //TextBox m_TextboxPassword = new TextBox();
-        //Label m_LabelPlayers = new Label();
+        //Label LabelPlayers = new Label();
         //Label m_LabelPlayer1 = new Label();
         //Label m_LabelPlayer2 = new Label();
         //Button m_ButtonOK = new Button();
@@ -34,25 +42,46 @@ namespace DamkaGUI
 
         private void initControls()
         {
-            this.m_LabelBoardSize.Text = "Board Size:";
-            this.m_LabelBoardSize.Location = new Point(10, 20);
+            
 
-            this.initRadioButtons();
+            this.initBoardSizeSection();
+            this.initPlayersSection();
 
-            this.Controls.AddRange(new Control[] { m_LabelBoardSize});
+            this.Controls.AddRange(new Control[] { LabelBoardSize});
 
         }
 
-        private void initRadioButtons()
+        private void initPlayersSection()
         {
-            for(int i = 0; i < 3; i++)
+            int baseVerticalHight = 70;
+
+            this.LabelPlayers.Text = "Players:";
+            this.LabelPlayers.Location = new Point(10, baseVerticalHight);
+            this.LabelPlayer1.Text = "Player 1:";
+            this.LabelPlayer1.Location = new Point(this.r_HorizontalMargin, baseVerticalHight + this.r_VeticalMergin);
+            this.Player1Name.Location = new Point(100, this.LabelPlayer1.Location.Y - 3);
+            this.CheckBoxPlayer2.Text = "Player 2:";
+            this.CheckBoxPlayer2.Location = new Point(this.r_HorizontalMargin, baseVerticalHight + this.r_VeticalMergin*2);
+            this.Player2Name.Location = new Point(100, this.CheckBoxPlayer2.Location.Y + 3);
+            this.Player2Name.Enabled = false;
+            this.Player2Name.Text = "[Computer]";
+
+            this.Controls.AddRange(new Control[] {this.Player2Name, this.Player1Name ,this.LabelPlayers, this.LabelPlayer1, this.CheckBoxPlayer2 });
+        }
+
+        private void initBoardSizeSection()
+        {
+            this.LabelBoardSize.Text = "Board Size:";
+            this.LabelBoardSize.Location = new Point(10, 20);
+
+            for (int i = 0; i < 3; i++)
             {
                 RadioButton radioButton = new RadioButton();
                 String label = $"{(i + 3) * 2}X{(i + 3) * 2}";
                 radioButton.Text = label;
                 radioButton.Size = new Size(55, 30);
-                radioButton.Location = new Point(10 + (i * radioButton.Size.Width), 40);
-                m_RadioButtonsBoardSize.Add(label, radioButton);
+                radioButton.Location = new Point(this.r_HorizontalMargin + (i * radioButton.Size.Width), 40);
+                RadioButtonsBoardSize.Add(label, radioButton);
                 this.Controls.Add(radioButton);
             }
         }
